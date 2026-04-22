2dabc3bd3ec5e96e43693745d643b23bc7b9a179
Rodel Agcaoili <ragcaoili3@gatech.edu>
2026-04-21 22:01:26 -0400

    fix: change to valid model and modify iam policy

--- projects/01-secure-rag/layers.tf
+++ projects/01-secure-rag/layers.tf
@@ -1,14 +1,17 @@
-# Upload the ZIP to S3 instead of sending it via API
+data "archive_file" "faiss_layer_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/layer_build"
+  output_path = "${path.module}/faiss_layer.zip"
+}
+
 resource "aws_s3_object" "faiss_layer_artifact" {
   bucket = aws_s3_bucket.vector_store_bucket.id
   key    = "artifacts/faiss_layer_${random_id.suffix.hex}.zip"
   source = data.archive_file.faiss_layer_zip.output_path
   
-  # Ensure the ZIP is rebuilt before uploading
   depends_on = [data.archive_file.faiss_layer_zip]
 }
 
-# Update the Layer to point to the S3 Object
 resource "aws_lambda_layer_version" "faiss_layer" {
   layer_name = "faiss_ai_layer-${random_id.suffix.hex}"
   
@@ -17,6 +20,5 @@ resource "aws_lambda_layer_version" "faiss_layer" {
   
   compatible_runtimes = ["python3.12"]
   
-  # Keeps the layer updated if the zip content changes
   source_code_hash = data.archive_file.faiss_layer_zip.output_base64sha256
 }
